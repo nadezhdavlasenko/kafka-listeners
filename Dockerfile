4e9c6cb6001aaf16ee6e66053a31b79f9035eb89
Nadiia Vlasenko <ciklum.vlasenko@extaccount.com>
2020-07-22 19:22:14 +0300

    Run to Entrypoint

--- Dockerfile
+++ Dockerfile
@@ -3,4 +3,4 @@ FROM openjdk:8-jre-alpine
 #RUN mvn package
 COPY ./target/kafka-listeners-1.0-SNAPSHOT.jar /usr/app/
 WORKDIR /usr/app
-RUN java -jar "kafka-listeners-1.0-SNAPSHOT.jar"
+ENTRYPOINT ["java", "-jar", "kafka-listeners-1.0-SNAPSHOT.jar"]
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,9 @@
 public class Main {
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws InterruptedException {
     while (true) {
       System.out.println("I'm running");
+      Thread.sleep(200);
     }
   }
 
